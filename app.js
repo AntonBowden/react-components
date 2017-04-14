29a297f14bd45a9e031cd0bcfb789410c2b93b3d
Anton Bowden <bowden.anton@gmail.com>
2017-04-15 00:12:21 +0100

    complete react components

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-// TODO
+var groceryItems = ['Apples', 'Bananas', 'Carrots'];
+
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onGroceryListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'underline' : 'none'
+    };
+    return (
+      <li style={style} onClick={this.onGroceryListItemClick.bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(item => <GroceryListItem item = {item} />)}
+  </ul>
+);
+
+
+ReactDOM.render(<GroceryList items={groceryItems}/>, document.getElementById("app"));
